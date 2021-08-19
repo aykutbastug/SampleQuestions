b72556f0ac5e8f2e51756586723c7d661cb1a925
Aykut BAŞTUĞ <aykutbastug@gmail.com>
2021-08-19 20:31:02 +0300

    .

--- SampleQuestion2/Program.cs
+++ SampleQuestion2/Program.cs
@@ -12,7 +12,7 @@ namespace SampleQuestion2
             binNumbers.Add(new BinNumber() { Bin = "529572", Banka = "FINANSBANK", KartProgrami = "FIX", MaksimumTaksitSayisi = 12 });
             binNumbers.Add(new BinNumber() { Bin = "535280", Banka = "AKBANK", KartProgrami = "AXESS", MaksimumTaksitSayisi = 9 });
             binNumbers.Add(new BinNumber() { Bin = "435508", Banka = "AKBANK", KartProgrami = "WINGS", MaksimumTaksitSayisi = 6 });
-            binNumbers.Add(new BinNumber() { Bin = "435508", Banka = "GARANTİ", KartProgrami = "BONUS", MaksimumTaksitSayisi = 10 });
+            binNumbers.Add(new BinNumber() { Bin = "435507", Banka = "GARANTİ", KartProgrami = "BONUS", MaksimumTaksitSayisi = 10 });
             binNumbers.Add(new BinNumber() { Bin = "403666", Banka = "GARANTİ", KartProgrami = "BONUS BUSINESS", MaksimumTaksitSayisi = 10 });
             binNumbers.Add(new BinNumber() { Bin = "520097", Banka = "GARANTİ", KartProgrami = "BONUS PLATINUM", MaksimumTaksitSayisi = 3 });
             binNumbers.Add(new BinNumber() { Bin = "540122", Banka = "YAPIKREDİ", KartProgrami = "CLASSIC", MaksimumTaksitSayisi = 9 });
